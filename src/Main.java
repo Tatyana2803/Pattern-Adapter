843c7b51f0f749dcfa65b10b0cf014d8559b4a6a
Кондакова Татьяна <rotaru2803@rambler.ru>
2022-04-26 13:53:18 +0500

    add java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+
+        System.out.println(calc.mult(5, 4));
+        System.out.println(calc.pow(10, 2));
+        System.out.println(calc.sum(0, 5));
+    }
+}
